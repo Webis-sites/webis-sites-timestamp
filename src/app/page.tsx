e32b9f3614558b4d560be77d6b7b3a6362f0b442
Idan Harari <84401044+IdanHarari11@users.noreply.github.com>
2025-04-01 13:25:27 +0300

    Add src/app/page.tsx to project

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import ServicesSection from '../components/ServicesSection';
+import ValueProposition from '../components/ValueProposition';
 
 import React from 'react';
 
@@ -9,7 +9,7 @@ export default function Home() {
     <main className="min-h-screen flex flex-col items-center p-8">
       <h1 className="text-4xl font-bold mb-8">ברוכים הבאים</h1>
       {/* רכיבים */}
-      <ServicesSection />
+      <ValueProposition />
       
     </main>
   );
